13c28d7e3c72de7e8b12214c486e3a99706f31da
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-09-07 04:57:14 +0530

    Home PAge

---
+++ Home/script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-button').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active'); // Toggle the active class
+});
