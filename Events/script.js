dcef9d7449f531dfa3f117cb341d894a4d5c5406
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-09-10 08:26:23 +0530

    Updated

--- Events/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.cta-button').forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default link behavior
-        alert('Thank you for registering for the event!'); // Simple alert for registration confirmation
-    });
-});
--- Networking-Hub/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active'); // Toggle the active class
-});
